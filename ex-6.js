829519e47a9f490df33a282343eb02103dcf073b
Nok <siriwan.skk@gmail.com>
2023-11-23 15:33:01 +0700

    feat: ex2 finish

--- ex-6.js
+++ ex-6.js
@@ -2,5 +2,19 @@
 
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+let concatSentence = "Total: " + totalParts + " Machine parts";
+let literalSentence = `Total: ${totalParts} Machine parts`;
+
+console.log(concatSentence);
+console.log(literalSentence);
+
+// let alertMessage1 = 'Warning! This is an "alert" message.';
+// console.log(alertMessage1);
+
+// let alertMessage2 = "Warning! This is an \"alert\" message.";
+// console.log(alertMessage2);
+
+// let a = 1;
+// let b = 2;
+// let message = `The result is ${a + b}`;
+// console.log(message);
